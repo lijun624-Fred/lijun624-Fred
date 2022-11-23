d323027bad0daa40c17be737886ef67eff57b0a2
LI Fred (CI/PIP3) <ilf2szh@bosch.com>
2022-11-23 17:09:33 +0800

    run well

--- tojson.py
+++ tojson.py
@@ -7,12 +7,10 @@ with open('template.json', mode='r') as f:
  
     # 将 JSON 对象转换为 Python 字典
     data2 = json.load(f)
-    # print (data2)
 
     # print (data2['resources'][0]['properties']['virtualNetworkPeerings'])
     for peer in data2['resources'][0]['properties']['virtualNetworkPeerings']:
-        print(peer['name'])
-        print(peer['properties']['remoteAddressSpace']['addressPrefixes'][0])
-    # data_flat = [dict(id=x["id"], **x["properties"]) for x in data2]
-    # df = pd.DataFrame(data_flat)
-    # print(df)
+        # print(peer['name'])
+        # print(peer['properties']['remoteAddressSpace']['addressPrefixes'][0])
+        temp_name = peer['properties']['remoteVirtualNetwork']['id'][29:]
+        print(temp_name[:-14])
