dd4856d8b6ef1b178765e79efd1ca52a01c4dc60
LI Fred (BD/PIP3) <ilf2szh@bosch.com>
2023-05-29 13:14:48 +0800

    add backend

--- tffordemo/backend.tf
+++ tffordemo/backend.tf
@@ -1,10 +1,17 @@
 terraform {
   # required_version = ">= 0.12.0"  
-  #  backend "azurerm" { }
+  # backend "azurerm" { }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=3.15.0"
     }
   }
+  # backend "azurerm" {
+  #   resource_group_name  = "xxx"
+  #   storage_account_name = "xxx"
+  #   container_name       = "xxx"
+  #   key                  = "xxx"
+  #   environment          = "china"
+  # }
 }
